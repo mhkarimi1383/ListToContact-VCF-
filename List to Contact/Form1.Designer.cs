2f776741c2b008f27e8c6767d8b1e7a021bf8856
Muhammed Hussein <m.h.karimi1383@outlook.com>
2019-01-24 21:01:49 +0330

    Almost Done.

---
+++ List to Contact/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace List_to_Contact
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 578);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 21;
+            this.listBox2.Location = new System.Drawing.Point(308, 0);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(306, 578);
+            this.listBox2.TabIndex = 0;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 21;
+            this.listBox3.Location = new System.Drawing.Point(614, 0);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(308, 578);
+            this.listBox3.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox2);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.listBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 141);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(922, 578);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 53);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(922, 88);
+            this.panel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 88);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Number List";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(308, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(306, 88);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Open First Name List";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.Image = global::List_to_Contact.Properties.Resources.icons8_opened_folder_100;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.Location = new System.Drawing.Point(614, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(308, 88);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Open Family Name List";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(922, 53);
+            this.panel3.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(711, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 53);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Select Output Folder";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(614, 0);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 53);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Save";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 719);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "List to Contact";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ List to Contact/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace List_to_Contact
+{
+    public partial class Form1 : Form
+    {
+        string pathToSave = string.Empty;
+        string VcfTemplate = File.OpenText($@"{Environment.CurrentDirectory}\VcfTemplate.txt").ReadToEnd();
+        FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        OpenFileDialog openFileDialog2 = new OpenFileDialog();
+        OpenFileDialog openFileDialog3 = new OpenFileDialog();
+        string numberList = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AjustSize();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            AjustSize();
+        }
+
+        private void AjustSize()
+        {
+            listBox1.Width = Width / 3;
+            listBox2.Width = Width / 3;
+            listBox3.Width = Width / 3;
+            button1.Width = Width / 3;
+            button2.Width = Width / 3;
+            button3.Width = Width / 3;
+            panel1.Height = Height - 200;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Reset();
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            folderBrowserDialog1.Description = "Select Folder to Save Contact Files";
+            DialogResult folderBrowserDialog1DialogResult = folderBrowserDialog1.ShowDialog();
+            if (folderBrowserDialog1DialogResult == DialogResult.OK)
+            {
+                pathToSave = folderBrowserDialog1.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show(this, "You are not selected a path!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            folderBrowserDialog1.Reset();
+        }
+
+        //Open Number List
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Reset();
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Title = "Select Number List File";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            DialogResult openFileDialog1DialogResult = openFileDialog1.ShowDialog();
+            if (openFileDialog1DialogResult == DialogResult.OK)
+            {
+                listBox2.Items.Clear();
+                StreamReader numbersListStreamReader = new StreamReader(openFileDialog1.FileName);
+                string line;
+                while ((line = numbersListStreamReader.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(line);
+                }
+                numberList = numbersListStreamReader.ReadToEnd();
+                numbersListStreamReader.Close();
+            }
+            else
+            {
+                MessageBox.Show(this, "You are not selected a File!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Reset();
+            openFileDialog2.CheckFileExists = true;
+            openFileDialog2.CheckPathExists = true;
+            openFileDialog2.Title = "Select First Name List File";
+            openFileDialog2.Filter = "txt files (*.txt)|*.txt";
+            DialogResult openFileDialog2DialogResult = openFileDialog2.ShowDialog();
+            if (openFileDialog2DialogResult == DialogResult.OK)
+            {
+                listBox2.Items.Clear();
+                StreamReader firstNamesListStreamReader = new StreamReader(openFileDialog2.FileName);
+                string line;
+                while ((line = firstNamesListStreamReader.ReadLine()) != null)
+                {
+                    listBox2.Items.Add(line);
+                }
+                firstNamesListStreamReader.Close();
+            }
+            else
+            {
+                MessageBox.Show(this, "You are not selected a File!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog3.Reset();
+            openFileDialog3.CheckFileExists = true;
+            openFileDialog3.CheckPathExists = true;
+            openFileDialog3.Title = "Select Last Name List File";
+            openFileDialog3.Filter = "txt files (*.txt)|*.txt";
+            DialogResult openFileDialog3DialogResult = openFileDialog3.ShowDialog();
+            if (openFileDialog3DialogResult == DialogResult.OK)
+            {
+                listBox3.Items.Clear();
+                StreamReader lastNamesListStreamReader = new StreamReader(openFileDialog3.FileName);
+                string line;
+                while ((line = lastNamesListStreamReader.ReadLine()) != null)
+                {
+                    listBox3.Items.Add(line);
+                }
+                lastNamesListStreamReader.Close();
+            }
+            else
+            {
+                MessageBox.Show(this, "You are not selected a File!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            int counter=0;
+            int countToFinish = listBox1.Items.Count;
+            while (counter < countToFinish)
+            {
+                StreamWriter contactFileStreamWriter = new StreamWriter($@"{pathToSave}\{counter}.vcf");
+                contactFileStreamWriter.AutoFlush = true;
+                contactFileStreamWriter.Write(VcfTemplate
+                    .Replace("<Number>", listBox1.Items[counter].ToString())
+                    .Replace("<FirstName>", listBox2.Items[counter].ToString())
+                    .Replace("<LastName>", listBox3.Items[counter].ToString()));
+                contactFileStreamWriter.Close();
+            }
+        }
+    }
+}
